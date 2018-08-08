5c109ecdaa765e7f1620a6d6e41dbbf7d8ec68a2
Ran Guin <ran.guin@gmail.com>
2018-08-08 15:33:42 -0700

    simplify main page

--- Main.vue
+++ Main.vue
@@ -15,7 +15,7 @@
         span &nbsp; &nbsp;
         p &nbsp;
 
-      div.onPage
+      div
         p &nbsp;
         Interests(v-if="show==='Interests'" :list='interests')
         Events(v-if="show==='Events'" :list='events')
@@ -133,114 +133,18 @@ export default {
 
 .onPage {
   color: black;
-  background-color: lightgreen !important;
+  background-color: grey !important;
 }
 .onPage:hover {
   color: black;
-  background-color: lightgreen !important;
+  background-color: grey !important;
 }
 .offPage {
-  background-color: #eee !important;
+  background-color: grey !important;
+  color: white;
 }
 .offPage:hover {
-  background-color: #aaa !important;
+  background-color: darkgrey !important;
+  color: black;
 }
-
-.page {
-  /*margin-top: -20px;*/
-  height: 100%;
-  width: 100%;
-}
-.body {
-  min-height: calc(100vh - 150px);
-}
-.header {
-  height: 80px;
-  background-color: #3a3;
-}
-.footer {
-  height: 70px;
-  /*padding: 10px;*/
-  background-color: #ccc;
-}
-
-  .body {
-    height: auto;
-  }
-  .pageWrapper {
-    margin: 0px;
-  }
-
-  .flexWrapper {
-    display: flex;
-    flex-flow: row no-wrap;
-  }
-
-  .flexChild {
-    flex: 1;
-    justify-content: space-around;
-  }
-
-  .mainBlock {
-    padding: 20px;
-    align-content: top;
-  }
-
-  .user-section, .scheduled-section, .coverage-section {
-    width: 80%;
-    margin-left: 10%;
-    margin-right: 10%;
-    margin-top: 40px;
-    border: 1px solid black;
-    padding: 10px;
-  }
-
-  .block {
-    padding: 0px;
-    /*border: 1px solid black;*/
-    min-height: 200px;
-  }
-  .block-header {
-    padding: 10px;
-    background-color: #ccc;
-  }
-  .block-subheader {
-    padding: 10px;
-    background-color: #ddd;
-  }
-  .block-body {
-    padding: 20px;
-    background-color: #eee;
-  }
-  .block-footer {
-    padding: 20px;
-    background-color: '#eee';
-  }
-  .coverageBlock {
-    border: solid black 3px;
-    padding: 20px;
-    min-width: 200px;
-    max-width: 400px;
-  }
-  .mainBlock {
-    border-top: solid black 3px;
-    border-right: solid black 3px;
-    padding: 20px;
-    transition: opacity 1s ease;
-  }
-  .mainSection {
-    transition: visibility 0s linear 0s, opacity 1000ms;
-  }
-
-  .footer {
-    padding-left: 40px;
-    padding-right: 40px;
-    padding-top: 15px;
-    padding-bottom: 15px;
-  }
-  .logo-s {
-    max-height: 70px;
-    margin-top: -15px;
-    padding-right: 50px;
-  }
 </style>
