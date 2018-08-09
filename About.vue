3978dcfbadc28f3e72a3722781ecae7183479a19
Ran Guin <ran.guin@gmail.com>
2018-08-08 19:55:07 -0700

    tidy up main pages

--- About.vue
+++ About.vue
@@ -1,19 +1,23 @@
 <template lang='pug'>
     div.about
       div.container
-        h3 How it Works:
         div.col-md-6
+          h3 How it Works:
           u
             h4 Participate
           ul
             li(v-for='step in setup')
               i {{step}}
-        div.col-md-6
           u
             h4 Host
           ul
             li(v-for='step in hosting')
               i {{step}}
+        div.col-md-6
+          h3 How is it different?
+          ul
+            li(v-for='diff in differences')
+              i {{diff}}
 </template>
 
 <script>
@@ -25,7 +29,8 @@ export default {
   data () {
     return {
       setup: config.setup,
-      hosting: config.hosting
+      hosting: config.hosting,
+      differences: config.demo_differences
     }
   },
   props: {
