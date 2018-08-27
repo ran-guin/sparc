7e380b7317412f6ef882216927fe758b60bf09bb
Ran Guin <ran.guin@gmail.com>
2018-08-27 12:02:21 -0700

    add public version of pages

--- Interests.vue
+++ Interests.vue
@@ -1,16 +1,26 @@
 <template lang='pug'>
     div.container
-      span(v-for='page in pages' style='padding-left: 50px')
+      div(v-show="!payload || !payload.userid")
+        p Select from a broad list of specific activities & interests within a few fundamental categories.
+        p Members can also propose new event/activity subtypes so this list can continue to expand to meet community needs
+        hr
+      span(v-for='page in pages' style='padding-left: 50px; border 1px solid black')
         a(href='#' @click.prevent="show=page" v-bind:class="[{onPage: show===page}, {offPage: show!==page}]")
           b.submenu {{page}}
       p &nbsp;
-      h3 Specify Interests
-      div(v-show="show==='Sport'")
-        RecursiveList(:list='sport' :onSelect='skillModal' :options='options')
-      div(v-show="show==='Cultural'")      
-        RecursiveList(:list='cultural' :onSelect='skillModal' :options='options')
+      <!-- h3 Specify Interests -->
       div(v-show="show==='Social'")
-        RecursiveList(:list='social' :onSelect='skillModal' :options='options')
+        u
+          h4 Social Interests:
+        RecursiveList(:list='social' :onSelect='skillModal' :selected='interest_ids' :options='listOptions')
+      div(v-show="show==='Sport'")
+        u
+          h4 Sporting Interests:
+        RecursiveList(:list='sport' :onSelect='skillModal' :selected='interest_ids' :options='listOptions')
+      div(v-show="show==='Cultural'")
+        u 
+          h4 Cultural Interests:
+        RecursiveList(:list='cultural' :onSelect='skillModal' :selected='interest_ids' :options='listOptions')
 </template>
 
 <script>
@@ -26,48 +36,35 @@ export default {
     return {
       pages: ['Social', 'Sport', 'Cultural'],
       show: 'Social',
-      options: {
-        selectable: true,
-        showSelect: this.definedSkill,
-        selectText: '- [experience level]'
-      },
       split_list: {},
       sections: [],
       aliases: {}
     }
   },
   props: {
-    list: { type: Array }
+    list: { type: Array },
+    interest_ids: { type: Array },
+    payload: { type: Object }
   },
   created: function () {
-    var inList = {}
-    for (var i = 0; i < this.list.length; i++) {
-      var interest = this.list[i]
-      var id = interest.id
-      var parent = interest.parent_id
-      var name = interest.name
-
-      if (interest.skill_level) {
-        interest.alias = interest.name + ' [' + interest.skill_level + ']'
-      } else {
-        interest.alias = interest.name
-      }
-      var section = inList[parent] || name
-      inList[id] = section
-
-      if (parent && inList[parent]) {
-        this.split_list[section].push(interest)
-        console.log('add ' + interest.name + ' to ' + section)
-      } else {
-        this.split_list[section] = [interest]
-        this.sections.push(name)
-        console.log(section + ' section added for ' + name)
-      }
-    }
-    console.log('generated sections: ' + this.sections.join(', '))
-    console.log('generated split_list: ' + JSON.stringify(this.split_list))
+    this.subdivideList()
   },
   computed: {
+    listOptions: function () {
+      var options = {
+        selectable: true,
+        showSelect: this.definedSkill,
+        selectText: '- [experience level]'
+      }
+      if (this.payload && this.payload.userid) {
+        options.open = 'selected'
+        options.hide = true
+      } else {
+        options.open = 'all'
+      }
+
+      return options
+    },
     sport: function () {
       console.log('retrieved sport list: ')
       var list = this.split_list['sport']
@@ -86,6 +83,7 @@ export default {
       console.log(JSON.stringify(list))
       return list
     }
+
     // aliases: function () {
     //   var aliases = []
     //   for (var i = 0; i < this.list.length; i++) {
@@ -101,7 +99,40 @@ export default {
     //   return aliases
     // }
   },
+  watch: {
+    interest_ids: function (val) {
+      console.log('WATCHED interest_ids: ' + val)
+    }
+  },
   methods: {
+    subdivideList: function () {
+      var inList = {}
+      for (var i = 0; i < this.list.length; i++) {
+        var interest = this.list[i]
+        var id = interest.id
+        var parent = interest.parent_id
+        var name = interest.name
+
+        if (interest.skill_level) {
+          interest.alias = interest.name + ' [' + interest.skill_level + ']'
+        } else {
+          interest.alias = interest.name
+        }
+        var section = inList[parent] || name
+        inList[id] = section
+
+        if (parent && inList[parent]) {
+          this.split_list[section].push(interest)
+          // console.log('add ' + interest.name + ' to ' + section)
+        } else {
+          this.split_list[section] = [interest]
+          this.sections.push(name)
+          console.log(section + ' section added for ' + name)
+        }
+      }
+      console.log('generated sections: ' + this.sections.join(', '))
+      console.log('generated split_list: ' + JSON.stringify(this.split_list))
+    },
     definedSkill: function (record) {
       if (record.skill_level) {
         return true
