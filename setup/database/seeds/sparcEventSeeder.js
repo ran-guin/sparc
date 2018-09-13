5c571df9dca5ad11b356242f2a0081a17108c9dd
Ran Guin <ran.guin@gmail.com>
2018-09-13 12:43:29 -0700

    tweak event seeder

--- setup/database/seeds/sparcEventSeeder.js
+++ setup/database/seeds/sparcEventSeeder.js
@@ -16,13 +16,13 @@ const Database = use('Database')
 class sparcEventSeeder {
   async run () {
   	const events = await Database.table('event').insert([
-          {id: 1, name: 'Potluck', description: 'description of potluck', location: 'Toronto, ON', interest_id: 18},
-          {id: 2, name: 'Swing Dancing', description: 'description of dance', location: 'Vancouver', interest_id: 28},
-          {id: 3, name: 'Dinner Party', description: 'description of art gallery event', location: 'Toronto, ON', interest_id: 17},
-          {id: 4, name: 'Bike Ride', description: 'description of art gallery event', location: 'Toronto, ON', interest_id: 11},
-          {id: 5, name: 'Art Gallery', description: 'description of art gallery event', location: 'Toronto, ON', interest_id: 24},
-          {id: 6, name: 'Flash Mob', description: 'description of dance', location: 'Vancouver', interest_id: 27},
-          {id: 7, name: 'Singles book club', description: 'description of dance', location: 'Vancouver', interest_id: 39}
+          {id: 1, name: 'Potluck', description: 'description of potluck', location: 'Toronto, ON'},
+          {id: 2, name: 'Swing Dancing', description: 'description of dance', location: 'Vancouver'},
+          {id: 3, name: 'Dinner Party', description: 'description of art gallery event', location: 'Toronto, ON'},
+          {id: 4, name: 'Bike Ride', description: 'description of art gallery event', location: 'Toronto, ON'},
+          {id: 5, name: 'Art Gallery', description: 'description of art gallery event', location: 'Toronto, ON'},
+          {id: 6, name: 'Flash Mob', description: 'description of dance', location: 'Vancouver'},
+          {id: 7, name: 'Singles book club', description: 'description of dance', location: 'Vancouver'}
   	])
 
   	console.log('seeded events: ' + events)
