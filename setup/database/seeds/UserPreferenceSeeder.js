31512b60eb3997241ba9179999de0829bed99de1
Ran Guin <ran.guin@gmail.com>
2018-09-13 13:28:32 -0700

    add user preference seeds

---
+++ setup/database/seeds/UserPreferenceSeeder.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| UserPreferenceSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+const Factory = use('Factory')
+
+class UserPreferenceSeeder {
+  async run () {
+  }
+}
+
+module.exports = UserPreferenceSeeder
