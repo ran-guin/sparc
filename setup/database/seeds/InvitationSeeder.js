67c11bb807c9a2fcedf4902d73917f4e152d1d11
Ran Guin <ran.guin@gmail.com>
2018-09-14 16:28:02 -0700

    add invitation seeder

--- setup/database/seeds/InvitationSeeder.js
+++ setup/database/seeds/InvitationSeeder.js
@@ -11,9 +11,22 @@
 */
 
 const Factory = use('Factory')
+const Database = use('Database')
 
 class InvitationSeeder {
   async run () {
+  	const invites = await Database.table('invitation').insert([
+          {user_id: 5, event_id: 2, status: 'invited'},
+          {user_id: 5, event_id: 3, status: 'invited'},
+          {user_id: 5, event_id: 4, status: 'accepted'},
+          {user_id: 5, event_id: 5, status: 'declined'},
+          {user_id: 5, event_id: 6, status: 'waiting list'},
+          {user_id: 5, event_id: 7, status: 'accepted', attendance: 'no show'},
+          {user_id: 5, event_id: 1, status: 'accepted', attendance: 'attended'},
+          {user_id: 4, event_id: 2},
+          {user_id: 4, event_id: 4},
+          {user_id: 7, event_id: 6}
+    ])
   }
 }
 
